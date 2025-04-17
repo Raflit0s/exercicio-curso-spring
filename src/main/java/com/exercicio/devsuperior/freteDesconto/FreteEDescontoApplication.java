4aed5903f98129c0f3a916b3af04484b4f33a1e5
Raflit0s <rafinhaprincipe36@gmail.com>
2025-04-16 21:27:02 -0300

    first commit

---
+++ src/main/java/com/exercicio/devsuperior/freteDesconto/FreteEDescontoApplication.java
@@ -0,0 +1,13 @@
+package com.exercicio.devsuperior.freteDesconto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreteEDescontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreteEDescontoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercicio/devsuperior/freteDesconto/FreteEDescontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercicio.devsuperior.freteDesconto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreteEDescontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
